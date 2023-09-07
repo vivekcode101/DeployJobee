7f2d2d49ba9cfb2ccfea65729c72dc5dd43f4a22
Vivek Kumar Yadav <vyadav@initializ.io>
2023-09-07 15:55:02 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const connectDatabase = () => { mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-    useCreateIndex : true
-}).then(con => {
-    console.log(`MongoDB Database connected with host: ${con.connection.host}`);
-    });
-};
+const connectDatabase = ()=>{mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ }).then(con=>{
+    console.log(`MongoDB Database connected with host: ${con.connection.host}`)
+ })
+}
 
-
-module.exports = connectDatabase;
+module.exports = connectDatabase
