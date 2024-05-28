0969a2b1874721f7beb7c1c8423b6b9e4a81f0c2
Vivek Yadav <vivekcode101@gmail.com>
2024-05-28 16:22:38 +0530

    user flag
    Signed-off-by: Vivek Yadav <vivekcode101@gmail.com>

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -18,7 +18,8 @@ const sendToken = (user, statusCode, res) => {
         .cookie('token', token, options)
         .json({
             success : true,
-            token
+            token,
+            role:user.role
         });
 
     
